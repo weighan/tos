348ae610883b9d7bd7bab1a7ff2262f395981dbc
jimmy <weighanlee@gmail.com>
2015-12-10 14:59:16 -0800

    finished com

--- kernel/com.c
+++ kernel/com.c
@@ -5,20 +5,13 @@ PORT com_port;
 void com_reader_process(PROCESS sef, PARAM param){
   COM_Message* msg;
   PROCESS com_proc;
-  int i;
+  int j;
   while(1){
     msg = receive(&com_proc);
-    //kprintf("msg from: %s\n", com_proc->name);
-    //kprintf("len is %d\n", msg->len_input_buffer);
-    for(i = 0; i < msg->len_input_buffer; i++){
+    for(j = 0; j < msg->len_input_buffer; j++){
       wait_for_interrupt(COM1_IRQ);
-      msg->input_buffer[i] = inportb(COM1_PORT);
-      //while(1);
-      kprintf("%c\n",msg->input_buffer[i]);
-      //kprintf("%c",inportb(COM1_PORT));
+      msg->input_buffer[j] = inportb(COM1_PORT);
     }
-    msg->input_buffer[i+1] = '\0';
-    kprintf("msging\n");
     message(com_port, msg);
   }
 }
@@ -34,27 +27,14 @@ void com_process(PROCESS sef, PARAM param){
     i=0;
     msg = receive(&client_proc);
     message(reader_port, msg);
-    kprintf("msg sent\n");
 
     while(msg->output_buffer[i] != 0){
-      //kprintf("%c \n", msg->output_buffer[i]);
-      if(msg->output_buffer[i] == 0){
-        kprintf("we got 0!\n");
-      }
       while (!(inportb(COM1_PORT+5) & (1<<5)));
       outportb(COM1_PORT,(unsigned char) msg->output_buffer[i]);
-      //kprintf("spit something out\n");
-      //while(1);
       i++;
     }
-    //while(1);
     receive(&reader);
-    kprintf("client is: %s\n", client_proc->name);
-    while(1);
-    //print_all_processes(kernel_window);
     reply(client_proc);
-    msg = NULL;
-    client_proc = NULL;
   }
 }
 
